cfbe7b42e7381b683e3c042b70dc0d36a198274b
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-03 13:28:20 +0530

    reactive form using form builder

--- src/app/forms/reactive-form/reactive-form.ts
+++ src/app/forms/reactive-form/reactive-form.ts
@@ -1,22 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-form.html',
   styleUrl: './reactive-form.css',
 })
 export class ReactiveForm {
 
-  loginForm = new FormGroup({
+  data: any;
 
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
-  });
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  // loginForm = new FormGroup({
+
+  //   email: new FormControl('', Validators.required),
+  //   password: new FormControl('', Validators.required)
+  // });
 
   onSubmit() {
-    console.log(this.loginForm.value);
+    //console.log(this.loginForm.value);
+    this.data = this.loginForm.value;
   }
 
 
