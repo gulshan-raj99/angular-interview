3ca13c8fbed69b58e00d94b5a96a1777af8f78b0
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-04 08:35:25 +0530

    Bind API response data into a table

---
+++ src/app/components/user-detail/user-detail.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { UserService } from '../../Service/user-service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-detail',
+  imports: [FormsModule],
+  templateUrl: './user-detail.html',
+  styleUrl: './user-detail.css',
+})
+export class UserDetail {
+
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+
+  }
+  loadUser() {
+    this.userService.getUser().subscribe(res =>{
+      this.users = res;
+    })
+  }
+
+}
