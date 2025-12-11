9d534f2104c88746d69a447ef95b8bd0b63ffebc
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-11 09:12:38 +0530

    pipe and gaurd

---
+++ src/app/customm-pipe-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CustommPipePipe } from './customm-pipe-pipe';
+
+describe('CustommPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CustommPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
