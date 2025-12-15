92088b8a2cea4ff14d2fceaf4f62258be34399ce
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-15 07:37:46 +0530

    custom pipe with example code

--- src/app/direactives/employee-direactive.ts
+++ src/app/direactives/employee-direactive.ts
@@ -1,14 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appEmployeeDireactive]'
 })
 export class EmployeeDireactive {
 
-  constructor(private el: ElementRef) {
-    el.nativeElement.style.background = '#ca3030ff';
-    el.nativeElement.style.padding = '10px';
-    el.nativeElement.style.borderRadius = '5px';
-   }
+  constructor(private el: ElementRef){}
 
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = '';
+  }
+
+  // use diractive <p appEmployeeDirective>Hover me</p>
 }
